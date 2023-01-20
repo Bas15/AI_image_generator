0a859047a6c8275178394e35b44a585a43e3f57c
Bas15 <bolajistephen72@gmail.com>
2023-01-20 13:49:15 +0100

    Open Ai generator backend

---
+++ index.js
@@ -0,0 +1,18 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+//enable body parser
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/openai', require('./routes/openaiRoutes'))
+
+app.listen(port, () => {
+    console.log(`server is connected ${port}`)
+})
