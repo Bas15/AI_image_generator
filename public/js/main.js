f38e7a3832f15dca8d245d8c422c395839d4ccb1
Awoleye Bolaji Stephen <33720231+Bas15@users.noreply.github.com>
2023-01-23 07:46:58 -0800

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,7 @@ function onSubmit(e) {
     try {
       showSpinner();
   
-      const response = await fetch('/openai/generateimage', {
+      const response = await fetch('/openai/generateImage', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
